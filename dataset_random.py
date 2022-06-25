cc594cbafb7f31e8dd34ebbcb18cd3bc4ec6f6b0
hwkanHW <hanwen.kang@qq.com>
2022-06-25 20:21:21 +0800

    new_update

--- dataset_random.py
+++ dataset_random.py
@@ -43,10 +43,14 @@ def read_points(file_path):
 def simple_segmentation(colors):
     seed = []
     for i in range(len(colors)):
-        if colors[i, 0] >= 0.6 and colors[i, 1] <= 0.4 and colors[i, 2] <= 0.4:
+        if colors[i, 0] >= 0.6 and colors[i, 1] <= 0.35 and colors[i, 2] <= 0.35:
             seed.append(i)
     return np.asarray(seed)
 
+def find_region(points):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    return pcd.get_max_bound(),pcd.get_min_bound()
 
 
 if __name__ == "__main__":
@@ -71,12 +75,18 @@ if __name__ == "__main__":
     seed_points = points[seed]
     for i in range(200):
         index = int(len(seed) * np.random.random([1, 1]))
-        [k, idx, _] = pcd_tree.search_knn_vector_3d(points[seed[i], :], 8000)
+        [k, idx, _] = pcd_tree.search_knn_vector_3d(points[index, :], 8000)
         train_pts = points[idx, :]
         train_lab = labels[idx]
-        if np.max(train_lab) > 1:
+        num_apple_points = len(np.where(train_lab!=1)[0])
+        # max_bound, min_bound = find_region(train_pts)
+        # max_x = max_bound[1]-min_bound[1]
+        # max_y = max_bound[2]-min_bound[2]
+        if np.max(train_lab) > 1 and num_apple_points>500:
             gt = cmap[train_lab, :]
             show3d.showpoints(train_pts, gt, ballradius=1)
+        else:
+            print("ss")
         # seed_neighbour.points = o3d.utility.Vector3dVector(points[idx, :])
         # seed_neighbour.colors = o3d.utility.Vector3dVector(colors[idx, :])
         # o3d.visualization.draw_geometries([seed_neighbour],
