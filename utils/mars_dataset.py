b167ed3161179e0e3e6a5548d03e76a777e7818e
hwkanHW <hanwen.kang@qq.com>
2022-06-26 00:17:14 +0800

    dataset load

--- utils/mars_dataset.py
+++ utils/mars_dataset.py
@@ -29,17 +29,28 @@ def pc_normalize(pc):
 
 
 class MarsDataset():
-    def __init__(self, batch_size=32, npoints=8000, normalize=True, color_channel=False, random_num=50):
+    def __init__(self, batch_size=32, npoints=8000, normalize=True, color_channel=False, random_num=5, load_data = True):
         self.batch_size = batch_size
         self.npoints = npoints
         self.normalize = normalize
         self.color_channel = color_channel
         self.random_num = random_num
         ##
+        self.batch_idx = 0
+        ##
         self.pts_set = []
         self.lab_set = []
-        pcd = o3d.geometry.PointCloud()
         ##
+        if load_data:
+            self.pts_data = np.load("pts_data.npy")
+            self.lab_data = np.load("cls_data.npy")
+        else:
+            self.load_data_from_pcd()
+        #
+        self.num_batches = (len(self.pts_data)) // self.batch_size
+
+    def load_data_from_pcd(self):
+        pcd = o3d.geometry.PointCloud()
         for i in range(len(label_list)):
             points, colors, labels = self._get_item(label_list[i])
             print(label_list[i])
@@ -69,7 +80,8 @@ class MarsDataset():
                     break
         self.pts_data = np.array(self.pts_set)
         self.lab_data = np.array(self.lab_set)
-
+        np.save('pts_data.npy', self.pts_data)
+        np.save('cls_data.npy', self.lab_data)
 
     def _get_item(self, file_path):
         points, colors = self.read_points(file_path)
@@ -111,7 +123,7 @@ class MarsDataset():
         return
 
     def reset(self):
-        return
+        self.batch_idx = 0
 
     def num_channel(self):
         if self.color_channel:
@@ -120,17 +132,26 @@ class MarsDataset():
             return 3
 
     def next_batch(self, augment=False):
-        return
+        start_idx = self.batch_size*self.batch_idx
+        end_idx = self.batch_size*(self.batch_idx+1)
+        pts = self.pts_data[start_idx:end_idx]
+        cls = self.lab_data[start_idx:end_idx]
+        self.batch_idx = self.batch_idx + 1
+        return pts, cls
 
     def has_next_batch(self):
-        return
+        return self.batch_idx < self.num_batches
 
     def return_dataset(self):
         return self.pts_data, self.lab_data
 
 
 if __name__ == "__main__":
-    dataset = MarsDataset()
-    pts, label = dataset.return_dataset()
-    print(pts.shape)
-    print(label.shape)
+    dataset = MarsDataset(batch_size=6,random_num=5,load_data=True)
+    pts_data, cls_data = dataset.return_dataset()
+    print(len(pts_data))
+    index = 0
+    while dataset.has_next_batch():
+        pts,cls = dataset.next_batch()
+        index = index+1
+    print(index)
